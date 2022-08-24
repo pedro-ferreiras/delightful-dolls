7c6bb1c3130e5faf8b999ee4f06a0e7c325b3923
Leith Atia <leith.atia@gmail.com>
2022-08-24 15:17:08 +0100

    created image column for Doll model and seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,11 @@ user1 = User.create!(first_name: "Pedro", last_name: "Ferreira", email: "pedro@d
 user2 = User.create!(first_name: "Leith", last_name: "Atia", email: "leith@dolls.com", password: "ilovedolls")
 user3 = User.create!(first_name: "Will", last_name: "May", email: "may@dolls.com", password: "ilovedolls")
 
-Doll.create!(name: "Annabelle", condition: "poor", description: "Slightly cursed. She's never where you expect her to be. Bad breath.  Doesn't like cats and cats don't like her. Apart from that, i think you'll be fine.", cursed: true, user_id: user1.id)
-Doll.create!(name: "Chucky", condition: "good", description: "asdfghjklasdfghjklsdfghjkdfghj",cursed: true, user_id: user1.id)
-Doll.create!(name: "Barbie", condition: "good", description: "asdfghjklasdfghjklsdfghjkdfghj", cursed: false, user_id: user2.id)
+Doll.create!(name: "Annabelle", condition: "poor", image: "https://res.cloudinary.com/dug5dmhjs/image/upload/v1661340401/production/annabelle_wyacea.jpg", description: "Slightly cursed. She's never where you expect her to be. Bad breath.  Doesn't like cats and cats don't like her. Apart from that, i think you'll be fine.", cursed: true, user_id: user1.id)
+Doll.create!(name: "Chucky", condition: "good", image: "https://res.cloudinary.com/dug5dmhjs/image/upload/v1661340401/production/chucky_byga0s.jpg", description: "Perfect for a night of terror and pure horror. Not for the feint hearted.",cursed: true, user_id: user1.id)
+Doll.create!(name: "Barbie", condition: "good", image: "https://res.cloudinary.com/dug5dmhjs/image/upload/v1661340401/production/barbie_yh9z5r.jpg", description: "A classic.", cursed: false, user_id: user2.id)
+Doll.create!(name: "Fiona", condition: "good", image: "https://res.cloudinary.com/dug5dmhjs/image/upload/v1661340401/production/doll_l79gta.jpg", description: "A friendly, well rounded doll.", cursed: false, user_id: user2.id)
+Doll.create!(name: "Maggie", condition: "poor", image: "https://res.cloudinary.com/dug5dmhjs/image/upload/v1661340401/production/creepy_doll_mug1gs.jpg", description: "Never, ever turn your back on Maggie. You'll never close both your eyes with her around.", cursed: true, user_id: user2.id)
+Doll.create!(name: "Joey", condition: "good", image: "https://res.cloudinary.com/dug5dmhjs/image/upload/v1661282164/production/doll_bg_coxdrr.png", description: "Joey, a new born doll. Perfect if you're thinking about having children although, unlike real kids, he won't make a mess.", cursed: false, user_id: user2.id)
 
 puts "finished"
