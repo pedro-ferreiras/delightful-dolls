acad5efa3c789af946b41c6090448cf08f4006a3
Pedro Ferreira <pedrom2ferreira@gmail.com>
2022-08-24 15:27:58 +0100

    cards_homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     redirect_to dolls_path if current_user
+    @dolls = Doll.all
   end
 end
